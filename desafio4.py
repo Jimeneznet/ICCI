46fe66589648f57c8d0974b36f863d03f1f503ad
Jimeneznet <55928792+Jimeneznet@users.noreply.github.com>
2020-03-14 13:08:00 -0300

    Update and rename desafio.py to desafio4.py

--- desafio4.py
+++ desafio4.py
@@ -1,3 +1,17 @@
+# (C) RODRIGO JIMÉNEZ 2020 GPLv3
+# ES DECIR LA COPIA DIRECTA DE ESTE CÓDIGO ES ILEGAL Y PENALIZADA POR LEY
+# CUALQUIER COPIA QUE PERJUDIQUE DIRECTAMENTE A QUIEN PUBLICA ORIGINALMENTE ESTE CODIGO
+# SERÁ INVESTIGADA Y SE TOMARÁN ACCIONES LEGALES CORRESPONDIENTES.
+
+###################################################################################
+
+#Desafío de Rodrigo Jiménez y Leandro Vergara
+
+
+
+
+
+
 
 #Cada paciente (in pacientes) = [hora,nombre,puls,presion,temp,resp, STATUS]
 pacientes = []
@@ -6,8 +20,11 @@ pacientesGraves = 0
 pacientesCriticos = 0
 pacientesEstablesHoraPunta = 0
 pacientesEnBloque = [0,0,0,0,0,0,0,0,0,0,0,0] #Rangos desde las 00:00 hasta las 23:59
+pulsosAnormalesBloque = [0,0,0,0,0,0,0,0,0,0,0,0] #Solo se guardarán los pacientes con pulso anormal
 
 
+#def es para defninir    ->   def f(x):
+#sino piensa que estas llamando la funcion, ej: y = f(x) <- si f(x) tiene un return
 def convertirHora(horaDecimal):
 
     horaOutput = int(horaDecimal) #La hora sin minut0s
@@ -16,45 +33,41 @@ def convertirHora(horaDecimal):
     return (horaEstandar)
 
 def clasificarBloques(hora):
-    global pacientesHorarioInhabil
+
+    idBloque = -1
 
     if(0 <= hora < 2):
-        pacientesEnBloque[0] += 1
-        pacientesHorarioInhabil += 1
+        idBloque = 0
     elif(2 <= hora < 4):
-        pacientesEnBloque[1] += 1
-        pacientesHorarioInhabil += 1
+        idBloque = 1
     elif(4 <= hora < 6):
-        pacientesEnBloque[2] += 1
-        pacientesHorarioInhabil += 1
+        idBloque = 2
     elif(6 <= hora < 8):
-        pacientesEnBloque[3] += 1
-        pacientesHorarioInhabil += 1
+        idBloque = 3
     elif(8 <= hora < 10):
-        pacientesEnBloque[4] += 1
+        idBloque = 4
     elif(10 <= hora < 12):
-        pacientesEnBloque[5] += 1
+        idBloque = 5
     elif(12<= hora < 14):
-        pacientesEnBloque[6] += 1
+        idBloque = 6
     elif(14 <= hora < 16):
-        pacientesEnBloque[7] += 1
+        idBloque = 7
     elif(16 <= hora < 18):
-        pacientesEnBloque[8] += 1
+        idBloque = 8
     elif(18 <= hora < 20):
-        pacientesEnBloque[9] += 1
+        idBloque = 9
     elif(20 <= hora < 22):
-        pacientesEnBloque[10] += 1
-        pacientesHorarioInhabil += 1
+        idBloque = 10
     elif(22 <= hora < 24):
-        pacientesEnBloque[11] += 1
-        pacientesHorarioInhabil += 1
+        idBloque = 11
+    return idBloque
     
-def BloqueMasPacientes():
+def BloqueMasPacientes(listaBloques):
 
-    cantidadMasPacientes = max(pacientesEnBloque)
+    cantidadMasPacientes = max(listaBloques)
     bloquesConIndice = []
 
-    for i, bloque in enumerate(pacientesEnBloque):
+    for i, bloque in enumerate(listaBloques):
         #Chequea recursivamente por los bloques
         #agrega a los bloques con respectivo indice de horario
         #Solo si es que son los que tienen más pacientes
@@ -65,12 +78,9 @@ def BloqueMasPacientes():
     
     return bloquesConIndice
 
-
 def ClasificarPaciente(paciente):
 
     actualStatus = 0 # 0 == error
-    global pacientesCriticos
-    global pacientesGraves
 
     #PULSO
     if(paciente[2] < 30 or paciente[2] > 150):
@@ -142,14 +152,13 @@ def ClasificarPaciente(paciente):
         if(status <= actualStatus):
             actualStatus = status
 
-    if(actualStatus == 2):
-        pacientesGraves += 1
-    elif(actualStatus == 1):
-        pacientesCriticos += 1
-
     return actualStatus
 
 def ProcesarArchivo():
+
+    global pacientesCriticos
+    global pacientesGraves
+
     archivo = open("pacientes.csv" , "r")
     for i, linea in enumerate(archivo): #enumerate() para saber en que linea estamos, con la variable i
         if(i > 0):  #La primera linea contiene los nombres de los datos
@@ -160,17 +169,37 @@ def ProcesarArchivo():
             paciente[4] = float(paciente[4].replace(",","."))#temp
             paciente[5] = float(paciente[5].replace(",","."))#respiración
             paciente.append(ClasificarPaciente(paciente)) # 1 critico; 2 grave; 3 medio; 4 estable  
-            #print(paciente[6]) #El status se guarda en el elemento 7
-            pacientes.append(paciente)
+            pacientes.append(paciente) #El status se guarda en el elemento 7
+            if(paciente[6] == 1):
+                #Si nuestro paciente tiene status = critico
+                pacientesCriticos += 1
+            elif(paciente[6] == 2):
+                #Si nuestro paciente está en estado grave
+                pacientesGraves += 1
     archivo.close()    
 
 def ProcesarPacientes():
     global pacientesEstablesHoraPunta
+    global pacientesHorarioInhabil
+
     for paciente in pacientes:
-        #Chequear horas
-        clasificarBloques(paciente[0])
         if(paciente[6] == 4 and 18 <= paciente[0] < 21): # Estable y en hora punta
             pacientesEstablesHoraPunta += 1
+            
+
+        #Chequear horas
+        bloquePaciente = clasificarBloques(paciente[0])
+        pacientesEnBloque[bloquePaciente] += 1
+        if not (bloquePaciente < 0 or bloquePaciente > 11):
+            if(bloquePaciente <= 3 or bloquePaciente >= 10):
+                pacientesHorarioInhabil += 1
+        
+        if not (60 < paciente[2] < 90):
+            #si no es (pulso Estable)
+            pulsosAnormalesBloque[bloquePaciente] += 1
+            
+        
+
 
 
 ProcesarArchivo()
@@ -188,10 +217,21 @@ print(f" - Porcentaje de pacientes graves: {porcentajeGraves}% \n - Porcentaje d
 print(f"Cantidad de personas estables en hora punta: { pacientesEstablesHoraPunta } \n")
 
 #Rango de horario con más pacientes
-rangosMasPacientes = BloqueMasPacientes()
+rangosMasPacientes = BloqueMasPacientes(pacientesEnBloque)
 print(f"Rango de horario con más pacientes: ")
 #Mostrar los rangos que tiene mas pacientes obtenidos con la funcion
 for rango in rangosMasPacientes:
     horaInicio = (rango[0]*2) -2
     horaTermino = rango[0]*2
     print(f" [{convertirHora(horaInicio)} - {convertirHora(horaTermino)}]: {rango[1]} pacientes.")
+
+#Rango de horario con más pacientes de pulso anormal
+rangosMasPulso = BloqueMasPacientes(pulsosAnormalesBloque)
+print(f"Rango de horario con más pacientes con pulso anormal: ")
+#Mostrar los rangos que tiene mas pacientes obtenidos con la funcion
+for rango in rangosMasPulso:
+    horaInicio = (rango[0]*2) -2
+    horaTermino = rango[0]*2
+    print(f" [{convertirHora(horaInicio)} - {convertirHora(horaTermino)}]: {rango[1]} pacientes con pulso anormal.")
+
+
